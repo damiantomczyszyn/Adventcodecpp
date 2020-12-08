b82a308d8dea6b177c18cfc3a1ff6ed6509518dc
damiantomczyszyn <65863645+damiantomczyszyn@users.noreply.github.com>
2020-12-08 09:18:38 +0100

    Dzień pierwszy
    Cyfry, których suma tworzy 2020 należy pomnożyć

---
+++ adventcode1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int mul=1;
+	int n=200;
+	int tab[200];
+	for (int i = 0; i < n; i++)
+	cin >> tab[i];
+	for (int i = 0; i < n; i++)
+		for (int j = i+1; j < n; j++)
+			for (int k = j+1; k < n; k++)
+			if (tab[i] + tab[j] + tab[k] == 2020) 
+			{
+				mul= tab[i] * tab[j]*tab[k];
+				break;
+			}
+	cout << endl << mul;
+}
+
